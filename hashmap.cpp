7b9a74341af95fcefd81ec7fb0549df208c20918
Lucas Rabello <lucasrabello2020@gmail.com>
2025-08-25 20:42:41 -0300

    Update

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <map>
 
-typedef struct Hashmap {
-    std::map<std::string, std::string> dados;
-};
-
-
 int main() {
 
-    
+    std::map<std::string, int> dados;
+    dados["nome1"] = 100;
+    dados["nome2"] = 200;
+    dados["nome1"] = 300;
+
+    for (auto dado : dados) {
+        std::cout << dado.first << " = " << dado.second << std::endl;
+    }
 
     return 0;
 }
