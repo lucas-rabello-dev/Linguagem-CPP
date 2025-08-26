8fba02c4fa9a05badd80570231457444e989bc82
Lucas Rabello <lucasrabello2020@gmail.com>
2025-08-26 20:06:23 -0300

    Update

--- Retomada/hashmap/hashmap.cpp
+++ Retomada/hashmap/hashmap.cpp
@@ -3,7 +3,28 @@
 
 int main() {
 
+    #include <iostream>
+#include <string>
+
+int simple_hash(const std::string& input) {
+    int sum = 0;
+    for (char c : input) {
+        sum += static_cast<int>(c); 
+    }
+    return sum;
+}
+
+
+int main() {
+    auto hash = simple_hash("Dev");
+    std::cout << hash << "\n";
     
+    return 0;
+}
+
+std::map<int, std::string> hashMap;
+
+hashMap[simpleHash("Dev")] = "Nosso valor";
 
     return 0;
 }
