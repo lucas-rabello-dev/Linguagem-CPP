5e65b0fef04054d6cad98826b78ac7a5ac5f7b72
Lucas Rabello <lucasrabello2020@gmail.com>
2025-08-28 17:28:11 -0300

    Update

---
+++ Retomada/type conversion/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Conversão de tipos
+    // Implicito = automático
+    // Explícito = precisa informar o novo tipo de dado
+
+    int x = 3.14;
+    cout << x; // saida: 3
+
+    double y = (int) 3.14;
+    cout << y; // saida: 3
+
+    // ASCII table
+    char z = 100;
+    cout << z; // saida: d
+    cout << (char) 100; // saida: d
+
+    int correct = 8;
+    int questions = 10;
+    double score = correct/questions * 100;
+    cout << score; // saida: 0
+    // isso acontece por que voce esta fazendo uma divisão com (int)
+    // double score = correct/(double)questions * 100; 
+
+
+    return 0;
+}
