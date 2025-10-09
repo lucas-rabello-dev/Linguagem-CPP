e181a28bee2122335ad6863a99884358a3b1ae2a
Lucas Rabello <lucasrabello2020@gmail.com>
2025-10-09 20:39:59 -0300

    Update simples

---
+++ Retomada/funçoes/sobrecarga de funções/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int soma(int n, int n2, int n3);
+int soma(int n, int n2);
+
+int main() {
+
+    cout << soma(2, 3, 4) << endl;
+
+    return 0;
+}
+
+
+int soma(int n, int n2, int n3) {
+    return n + n2 + n3;
+}
+
+int soma(int n, int n2) {
+    return n + n2;
+}
