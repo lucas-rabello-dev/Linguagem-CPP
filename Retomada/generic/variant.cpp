c2fa572e264f74fff3950acd09d0f704191ef4a3
Lucas Rabello <lucasrabello2020@gmail.com>
2025-10-15 12:50:17 -0300

    Update

--- Retomada/generic/variant.cpp
+++ Retomada/generic/variant.cpp
@@ -1,15 +1,17 @@
 // std::variant é como se fosse um "any"
 // é lendo mais funciona
 
+// compilar com -std=c++17
+
 
 #include <iostream>
 #include <variant>
-#include <typeinfo>
 
 // do C++17 para funcionar
 int main() {
     // variant que pode guardar int e string
     std::variant<int, std::string> valor;
     valor = "teste";
+    std::cout << "rodou";
     return 0;
 }
